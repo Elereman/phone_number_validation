199323871f73dcd56b267a4bcd1dc355ef105340
Elereman <123456mayday@gmail.com>
2021-01-25 17:55:07 +0200

    add get_it as di

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/cupertino.dart';
-import 'package:phone_number_validation/domain/usecase/validate_phone_number.dart';
+import 'package:phone_number_validation/back_bone/di.dart' as di;
 import 'package:phone_number_validation/flutter/bloc/home_page/bloc.dart';
 import 'package:phone_number_validation/flutter/page/home.dart';
 
 void main() {
+  di.init();
   runApp(MyApp());
 }
 
@@ -14,9 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       home: HomePage(
-        bloc: HomePageBloc(
-          validatePhoneNumberUseCase: DefaultValidatePhoneNumberUseCase(),
-        ),
+        bloc: di.sl.get<HomePageBloc>(),
       ),
     );
   }
